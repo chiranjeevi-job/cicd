d7a3c73d0de94dcf38e839d4f67721734a73276a
makam chiranjeevi <makamchiru123@gmail.com>
2026-01-19 17:27:05 +0530

    new thing added

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       <p>makam chiranjeevi shetty</p>
+      <p>git practice is happening</p>
     </>
   );
 }
